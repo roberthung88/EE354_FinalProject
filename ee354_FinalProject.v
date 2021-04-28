324c000493dfd56ea08f928c5b7c553bf346d5c0
Jui Po Hung <juipohun@usc.edu>
2021-04-28 00:18:28 -0700

    Update

--- ee354_FinalProject.v
+++ ee354_FinalProject.v
@@ -7,34 +7,35 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module ee354_FinalProject(Clk, CEN, Reset, Start, Ack, input_arr, det, q_I, q_Load, q_Comp, q_Done);
+module ee354_FinalProject(Clk, Reset, Start, Ack, input_arr, det, q_I, q_Enter, q_Load, q_Comp, q_Done);
 
 
 	/*  INPUTS */
-	input	Clk, CEN, Reset, Start, Ack;
-	input integer input_arr [7:0][7:0];
+	input	Clk, Reset, Start, Ack;
+	input [63:0] input_arr;
 	
 	
 	/*  OUTPUTS */
 	output reg [31:0] det;
 	// store current state
-	output q_I, q_Load, q_Comp, q_Done;
-	reg [3:0] state;	
-	assign {q_Done, q_Comp, q_Load, q_I} = state;
+	output q_I, q_Enter, q_Load, q_Comp, q_Done;
+	reg [4:0] state;	
+	assign {q_Done, q_Comp, q_Load, q_Enter, q_I} = state;
 	
 	
 	/* TEMP VARS */
-	reg integer seven [6:0][6:0];
-	reg integer six [5:0][5:0];
-	reg integer five [4:0][4:0];
-	reg integer four [3:0][3:0];
-	reg integer three [2:0][2:0];
-	reg integer temp_val[5:0]; // temp_val[0] stores det
+	integer seven [6:0][6:0];
+	integer six [5:0][5:0];
+	integer five [4:0][4:0];
+	integer four [3:0][3:0];
+	integer three [2:0][2:0];
+	integer temp_val [5:0]; // temp_val[0] stores det
 	reg [2:0] size_curr; //current size of matrix being worked on
 	reg [2:0] sub_index [4:0]; //column to "ignore" while calculating determinant
-
+	integer i, j, indexi, indexj, a, b, c, d, e, f, g, h, k;
+	
 	localparam 	
-	I = 4'b0001, LOAD = 4'b0010, COMP = 4'b0100, DONE = 4'b1000, UNK = 4'bXXXX;
+	I = 5'b00001, ENTER = 5'b00010, LOAD = 5'b00100, COMP = 5'b01000, DONE = 5'b10000, UNK = 5'bXXXXX;
 	
 	// NSL AND SM
 	always @ (posedge Clk, posedge Reset)
@@ -42,110 +43,125 @@ module ee354_FinalProject(Clk, CEN, Reset, Start, Ack, input_arr, det, q_I, q_Lo
 		if(Reset) begin
 			state <= I;		  	
 			size_curr = 3'bx;
-			sub_index = '{default:x}; 
-			temp_val = 'bx;
+			for(i = 0; i < 5; i=i+1) begin
+				sub_index[i] = 3'bx;
+			end
+			for(i = 0; i < 6; i=i+1) begin
+				temp_val[i] = 'x;
+			end
+			
 		end
 		else				
 			case(state)	
 				I:
 				begin
 					// state transfers on start condtion
-					if (Start) state <= LOAD;
+					state <= ENTER;
 					// data transfers
 					size_curr = 3'd7;
-					sub_index = '{default:0}; 
-					temp_val = '{default:0}; 
-				end		
+					for(i = 0; i < 5; i=i+1) begin
+						sub_index[i] = 3'b0;
+					end
+					for(i = 0; i < 6; i=i+1) begin
+						temp_val[i] = 0;
+					end
+						
+				end	
+				ENTER:
+				begin
+					if (Start) state <= LOAD;
+						
+				end	
 				LOAD:
 					begin
 						// state transfers
 						state <= COMP;
 						// data transfers
 						case(size_curr)
-							3'b000: //impossible case
-							3'b001: //impossible case
-							3'b010://impossible case must be at least a 3X3
+							3'b000: ;//impossible case
+							3'b001: ;//impossible case
+							3'b010: ;//impossible case must be at least a 3X3
 							3'b011: begin
-								integer i, j, indexi, indexj;
+								
 								indexi = 0;
 								indexj = 0;
-								for(i = 1; i < 4; i++) begin
-									for(j = 0; j < 4; j++) begin
+								for(i = 1; i < 4; i=i+1) begin
+									for(j = 0; j < 4; j=j+1) begin
 										if(j == sub_index[4])
-											j++;
+											j=j+1;
 										else begin
 											three[indexi][indexj] = four[i][j];
-											indexj++;
+											indexj=indexj+1;
 										end
 									end
-									indexi++;
+									indexi=indexi+1;
 								end
 							end
 							3'b100: begin
-								integer i, j, indexi, indexj;
+								
 								indexi = 0;
 								indexj = 0;
-								for(i = 1; i < 5; i++) begin
-									for(j = 0; j < 5; j++) begin
+								for(i = 1; i < 5; i=i+1) begin
+									for(j = 0; j < 5; j=j+1) begin
 										if(j == sub_index[3])
-											j++;
+											j=j+1;
 										else begin
 											four[indexi][indexj] = five[i][j];
-											indexj++;
+											indexj=indexj+1;
 										end
 									end
-									indexi++;
+									indexi=indexi+1;
 								end
 							end
 							3'b101: begin
-								integer i, j, indexi, indexj;
+								
 								indexi = 0;
 								indexj = 0;
-								for(i = 1; i < 6; i++) begin
-									for(j = 0; j < 6; j++) begin
+								for(i = 1; i < 6; i=i+1) begin
+									for(j = 0; j < 6; j=j+1) begin
 										if(j == sub_index[2])
-											j++;
+											j=j+1;
 										else begin
 											five[indexi][indexj] = six[i][j];
-											indexj++;
+											indexj=indexj+1;
 										end
 									end
-									indexi++;
+									indexi=indexi+1;
 								end
 							end
 							3'b110: begin
-								integer i, j, indexi, indexj;
+								
 								indexi = 0;
 								indexj = 0;
-								for(i = 1; i < 7; i++) begin
-									for(j = 0; j < 7; j++) begin
+								for(i = 1; i < 7; i=i+1) begin
+									for(j = 0; j < 7; j=j+1) begin
 										if(j == sub_index[1])
-											j++;
+											j=j+1;
 										else begin
 											six[indexi][indexj] = seven[i][j];
-											indexj++;
+											indexj=indexj+1;
 										end
 									end
-									indexi++;
+									indexi=indexi+1;
 								end
 							end
 							3'b111: begin
-								integer i, j, indexi, indexj;
+								
 								indexi = 0;
 								indexj = 0;
-								for(i = 1; i < 8; i++) begin
-									for(j = 0; j < 8; j++) begin
+								for(i = 1; i < 8; i=i+1) begin
+									for(j = 0; j < 8; j=j+1) begin
 										if(j == sub_index[0])
-											j++;
+											j=j+1;
 										else begin
-											seven[indexi][indexj] = input_arr[i][j];
-											indexj++;
+											seven[indexi][indexj] = input_arr[i*8+j];
+											indexj=indexj+1;
 										end
 									end
-									indexi++;
+									indexi=indexi+1;
 								end	
 							end
-							default: //hopefully we don't arrive here!
+							default: ;//hopefully we don't arrive here!
 						endcase
 
 					end
@@ -162,99 +178,99 @@ module ee354_FinalProject(Clk, CEN, Reset, Start, Ack, input_arr, det, q_I, q_Lo
 						
 						// data transfers
 						if(size_curr != 3)
-							size_curr--;
+							size_curr=size_curr-1;
 						else begin
-							integer a = three[0][0];
-							integer b = three[0][1];
-							integer c = three[0][2];
-							integer d = three[1][0];
-							integer e = three[1][1];
-							integer f = three[1][2];
-							integer g = three[2][0];
-							integer h = three[2][1];
-							integer i = three[2][2];
+							a = three[0][0];
+							b = three[0][1];
+							c = three[0][2];
+							d = three[1][0];
+							e = three[1][1];
+							f = three[1][2];
+							g = three[2][0];
+							h = three[2][1];
+							k = three[2][2];
 
 							// temp storage for 3x3 matrix det
-							temp_val[5] = a*(e*i-f*h) - b*(d*i-f*g) + c*(d*h-e*g);
+							temp_val[5] = a*(e*k-f*h) - b*(d*k-f*g) + c*(d*h-e*g);
 
 							// add det to 4x4
 							if(sub_index[4] % 2 == 0)
-								temp_val[4] += (temp_val[5]*four[0][sub_index[4]]);
+								temp_val[4] = temp_val[4]+(temp_val[5]*four[0][sub_index[4]]);
 							else
-								temp_val[4] -= (temp_val[5]*four[0][sub_index[4]]);
+								temp_val[4] = temp_val[4]-(temp_val[5]*four[0][sub_index[4]]);
 							
 
 							//calculated very last 3x3 matrix
 							if(sub_index[4] == 3) begin
 								sub_index[4] = 0;
-								size_curr++;
+								size_curr=size_curr+1;
 								// update values
 								if(sub_index[3] % 2 == 0)
-									temp_val[3] += (temp_val[4]*five[0][sub_index[3]]);
+									temp_val[3] = temp_val[3]+(temp_val[4]*five[0][sub_index[3]]);
 								else
-									temp_val[3] -= (temp_val[4]*five[0][sub_index[3]]);
+									temp_val[3] = temp_val[3]-(temp_val[4]*five[0][sub_index[3]]);
 								temp_val[4] = 0;
 
 								// calculated very last 4x4 matrix
 								if(sub_index[3] == 4) begin
 									sub_index[3] = 0;
-									size_curr++;
+									size_curr=size_curr+1;
 									// update values
 									if(sub_index[2] % 2 == 0)
-										temp_val[2] += (temp_val[3]*six[0][sub_index[2]]);
+										temp_val[2] = temp_val[2]+(temp_val[3]*six[0][sub_index[2]]);
 									else
-										temp_val[2] -= (temp_val[3]*six[0][sub_index[2]]);
+										temp_val[2] = temp_val[2]-(temp_val[3]*six[0][sub_index[2]]);
 									temp_val[3] = 0;
 
 									// calculated very last 5x5 matrix
 									if(sub_index[2] == 5) begin
 										sub_index[2] = 0;
-										size_curr++;
+										size_curr=size_curr+1;
 										// update values
 										if(sub_index[1] % 2 == 0)
-											temp_val[1] += (temp_val[2]*seven[0][sub_index[1]]);
+											temp_val[1] = temp_val[1]+(temp_val[2]*seven[0][sub_index[1]]);
 										else
-											temp_val[1] -= (temp_val[2]*seven[0][sub_index[1]]);
+											temp_val[1] = temp_val[1]-(temp_val[2]*seven[0][sub_index[1]]);
 										temp_val[2] = 0;
 
 										// calculated very last 6x6 matrix
 										if(sub_index[1] == 6) begin
 											sub_index[1] = 0;
-											size_curr++;
+											size_curr=size_curr+1;
 											// update values
 											if(sub_index[0] % 2 == 0)
-												temp_val[0] += (temp_val[1]*input_arr[0][sub_index[0]]);
+												temp_val[0] = temp_val[0]+(temp_val[1]*input_arr[sub_index[0]]);
 											else
-												temp_val[0] -= (temp_val[1]*input_arr[0][sub_index[0]]);
+												temp_val[0] = temp_val[0]-(temp_val[1]*input_arr[sub_index[0]]);
 											
 											// calculated very last 7x7 matrix
 											if(sub_index[0] == 7) begin
 												//calculated everything
-												temp_val[0] -= (temp_val[1]*input_arr[0][sub_index[0]]);
+												temp_val[0] = temp_val[0]-(temp_val[1]*input_arr[sub_index[0]]);
 												det <= temp_val[0];
 											end
 											else begin
 												temp_val[1] = 0;
-												sub_index[0]++;
+												sub_index[0]=sub_index[0]+1;
 											end
 											
 										end
 										else
-											sub_index[1]++;
+											sub_index[1]=sub_index[1]+1;
 										
 									end
 									else
-										sub_index[2]++;
+										sub_index[2]=sub_index[2]+1;
 									
 								end
 								else
 									// calculate next 4x4
-									sub_index[3]++;
+									sub_index[3]=sub_index[3]+1;
 								
 							end
 							else
 								// haven't calculated very last 3x3 matrix, so calculate next 3x3 det
-								sub_index[4]++;
+								sub_index[4]=sub_index[4]+1;
 						end
 					end
 				DONE:
---
+++ ee354_FinalProject_tb.v
@@ -0,0 +1,94 @@
+`timescale 1ns / 1ps
+
+module ee354_FinalProject_tb_v;
+
+	// Inputs
+	reg Clk;
+	reg Reset;
+	reg Start;
+	reg Ack;
+	integer input_arr [7:0][7:0];
+	
+
+	// Outputs
+	wire [31:0] det;
+	wire q_I;
+	wire q_Load;
+	wire q_Comp;
+	wire q_Done;
+	wire q_Enter;
+	reg [6*8:0] state_string;
+	integer clk_cnt, start_clock_cnt,clocks_taken;
+	// Instantiate the Unit Under Test (UUT)
+	ee354_FinalProject(.Clk(Clk), .Reset(Reset), .Start(Start), .Ack(Ack), .input_arr(input_arr), .det(det), .q_I(q_I), .q_Enter(q_Enter). .q_Load(q_Load), .q_Comp(q_Comp), .q_Done(q_Done));
+	
+		
+		
+		always  begin #5; Clk = ~ Clk; end
+		always@(posedge Clk) clk_cnt=clk_cnt+1; //don't want to use reset to clear the clk_cnt or initialize
+		initial begin
+		// Initialize Inputs
+		clk_cnt=0;
+		Clk = 0;
+		Reset = 0;
+		Start = 0;
+		Ack = 0;
+		
+		
+		//reset control
+		@(posedge Clk); //wait until we get a posedge in the Clk signal
+		@(posedge Clk);
+		#1;
+		Reset=1;
+		@(posedge Clk);
+		#1;
+		Reset=0;
+		
+		
+		//First stimulus (36,24)
+		input_arr = {{6, 1, 1, 0, 0, 0, 0, 0},
+             {4, -2, 5, 0, 0, 0, 0, 0},
+             {2, 8, 7, 0, 0, 0, 0, 0},
+             {0, 0, 0, 1, 0, 0, 0, 0},
+             {0, 0, 0, 0, 1, 0, 0, 0},
+             {0, 0, 0, 0, 0, 1, 0, 0},
+             {0, 0, 0, 0, 0, 0, 1, 0},
+             {0, 0, 0, 0, 0, 0, 0, 1}};
+		//make start signal active for one clock
+		@(posedge Clk);
+		#1;
+		Start=1;
+		@(posedge Clk);
+		#1;
+		Start=0;
+		//leaving the q_I state, so start keeping track of the clocks taken
+		start_clock_cnt=clk_cnt;
+		wait(q_Done); //wait until q_Done signal is a 1
+		clocks_taken = clk_cnt - start_clock_cnt;
+		#1;
+		$display("Determinant Value: %d", det);
+		$display("It took %d clock(s) to compute the GCD", clocks_taken);
+		//keep Ack signal high for one clock
+		Ack=1;
+		@(posedge Clk);
+		#1;
+		Ack=0;
+		
+		
+		
+	end
+	
+	always @(*)
+		begin
+			case ({q_I, q_Enter, q_Load, q_Comp, q_Done})    
+				5'b10000: state_string = "q_I   ";  
+				5'b01000: state_string = "q_Enter   ";  
+				5'b00100: state_string = "q_Load ";  
+				5'b00010: state_string = "q_Comp";
+				5'b00001: state_string = "q_Done";			
+			endcase
+		end
+ 
+      
+endmodule
+
