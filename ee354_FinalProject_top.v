ef9f1be19b2c87dd7e6e4b78f6e7af09bb810fba
Rob Sutherland <rfsuther@usc.edu>
2021-04-29 01:44:54 -0400

    Update ee354_FinalProject_top.v
    updated file

--- ee354_FinalProject_top.v
+++ ee354_FinalProject_top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Author:			Shideh Shahidi, Bilal Zafar, Gandhi Puvvada
 // Create Date:		02/25/08
-// File Name:		ee354_GCD_top.v 
+// File Name:		ee354_FinalProject.v 
 // Description: 
 //
 //
@@ -17,14 +17,14 @@
 
 `timescale 1ns / 1ps
 
-module ee354_GCD_top
+module ee354_FinalProject_top //modifed to use correct file (FINAL)
 		(MemOE, MemWR, RamCS, QuadSpiFlashCS, // Disable the three memory chips
 
         ClkPort,                           // the 100 MHz incoming clock signal
 		
 		BtnL, BtnU, BtnD, BtnR,            // the Left, Up, Down, and the Right buttons BtnL, BtnR,
 		BtnC,                              // the center button (this is our reset in most of our designs)
-		Sw15, Sw14, Sw13, Sw12, Sw11, Sw10, Sw9, Sw8, Sw7, Sw6, Sw5, Sw4, Sw3, Sw2, Sw1, Sw0, // 8 switches
+		Sw15, Sw14, Sw13, Sw12, Sw11, Sw10, Sw3, Sw2, Sw1, Sw0, // 8 switches
 		Ld7, Ld6, Ld5, Ld4, Ld3, Ld2, Ld1, Ld0, // 8 LEDs
 		An3, An2, An1, An0,			       // 4 anodes
 		An7, An6, An5, An4,                // another 4 anodes which are not used
@@ -37,7 +37,7 @@ module ee354_GCD_top
 	input		ClkPort;	
 	// Project Specific Inputs
 	input		BtnL, BtnU, BtnD, BtnR, BtnC;	
-	input		Sw15, Sw14, Sw13, Sw12, Sw11, Sw10, Sw9, Sw8, Sw7, Sw6, Sw5, Sw4, Sw3, Sw2, Sw1, Sw0;
+	input		Sw15, Sw14, Sw13, Sw12, Sw11, Sw10, Sw3, Sw2, Sw1, Sw0; //removed uselss switches
 	
 	
 	/*  OUTPUTS */
@@ -50,7 +50,6 @@ module ee354_GCD_top
 	output 	Cg, Cf, Ce, Cd, Cc, Cb, Ca, Dp;
 	output 	An0, An1, An2, An3;	
 	output 	An4, An5, An6, An7;	
-
 	
 	/*  LOCAL SIGNALS */
 	wire		Reset, ClkPort;
@@ -58,18 +57,31 @@ module ee354_GCD_top
 	wire [2:0] 	ssdscan_clk; //modified clk to include more SSDs (FINAL)
 	reg [26:0]	DIV_CLK;
 	
-	
-	wire Start_Ack_Pulse;
+	wire BtnL_Pulse; //was Start_Ack_Pulse (final)
 	wire in_AB_Pulse, CEN_Pulse, BtnR_Pulse, BtnU_Pulse; 
-	wire q_I, q_Load, q_Comp, q_Done; //changed this to include correct state (FINAL)
-	wire [7:0] A, B, AB_GCD, i_count;
-	reg integer input_arr [7:0][7:0]; //Added this for the input array (FINAL)
-	reg [31:0] det; // added this reg to hold det
-	reg [3:0]	SSD;
+	wire q_I, q_Enter, q_Load, q_Comp, q_Done; //changed this to include correct state (FINAL) 
+	//wire [7:0] A, B, AB_GCD, i_count; removed
+	reg [3:0] input_arr [63:0]; //Added this for the input array (FINAL) removed integer
+	reg [255:0] PackedInput_array ;
+	wire signed [31:0] det; // added this reg to hold det
+	// [31:28] [27:24] [23:20] [
+	reg [3:0]	SSD; //widened (final)
 	wire [3:0]	SSD7, SSD6, SSD5, SSD4,SSD3, SSD2, SSD1, SSD0; //added all SSDs (FINAL)
-	reg [7:0]  SSD_CATHODES; 
+	reg [7:0]  SSD_CATHODES;
 	
+	//added these new ones (FINAL)
+	wire [3:0] matrixInput;
+	wire [2:0] currRow;
+	wire [2:0] currColumn;
+	
+    integer i;
 //------------	
+
+
+
+
+
+
 // Disable the three memories so that they do not interfere with the rest of the design.
 	assign {MemOE, MemWR, RamCS, QuadSpiFlashCS} = 4'b1111;
 	
@@ -96,7 +108,7 @@ module ee354_GCD_top
 	// Our clock is too fast (100MHz) for SSD scanning
 	// create a series of slower "divided" clocks
 	// each successive bit is 1/2 frequency
-  always @(posedge board_clk, posedge Reset) 	
+  always @(posedge board_clk, posedge Reset) 	// trying to resolve multi assign operation
     begin							
         if (Reset)
 		DIV_CLK <= 0;
@@ -105,8 +117,8 @@ module ee354_GCD_top
     end
 //-------------------	
 	// In this design, we run the core design at full 100MHz clock!
-	assign	sys_clk = board_clk;
-	// assign	sys_clk = DIV_CLK[25];
+	//assign	sys_clk = board_clk;
+	assign	sys_clk = DIV_CLK[20]; //was 25
 
 //------------
 // INPUT: SWITCHES & BUTTONS
@@ -120,7 +132,7 @@ ee354_debouncer #(.N_dc(28)) ee354_debouncer_2
 		 // BtnR is used to generate in_AB_Pulse to record the values of need to modify this TODO(FINAL)
 		 // the inputs A and B as set on the switches.
 		 // BtnU is used as CEN_Pulse to allow single-stepping
-	assign {in_AB_Pulse, CEN_Pulse} = {BtnR_Pulse, BtnU_Pulse};
+	//assign {in_AB_Pulse, CEN_Pulse} = {BtnR_Pulse, BtnU_Pulse}; not used so i took it out (final)
 
 ee354_debouncer #(.N_dc(28)) ee354_debouncer_1 
         (.CLK(sys_clk), .RESET(Reset), .PB(BtnR), .DPB( ), 
@@ -130,50 +142,87 @@ ee354_debouncer #(.N_dc(28)) ee354_debouncer_0
         (.CLK(sys_clk), .RESET(Reset), .PB(BtnU), .DPB( ), // complete this instantiation
 		.SCEN(BtnU_Pulse), .MCEN( ), .CCEN( )); // to produce BtnU_Pulse from BtnU
 		
-//------------
-// DESIGN (FINAL)
+//------------ 
+//Seting inputs for diffrent Purposes here (FINAL)
+assign currRow = {Sw15, Sw14, Sw13};
+assign currColumn = {Sw12, Sw11, Sw10};
+assign matrixInput = {Sw3, Sw2, Sw1, Sw0};
+assign enterPulse =  BtnR_Pulse;
+
+// DESIGN (FINAL) DONT THINK WE NEED THIS ANYMORE SINCE WERE GOING TO INTILZE STATE ON RESET
+	// always @ (posedge sys_clk, posedge Reset)
+	// begin
+		// if(Reset)
+		// begin
+			////initialize matrix to identity matrix
+			// integer i, j;
+			// for(i = 0; i < 8; i++) begin
+				// for(j = 0; j < 8; j++) begin
+					// if(i == j)
+						// input_arr[i][i] <= 1;
+					// else
+						// input_arr[i][j] <= 0;
+				// end
+			// end
+		// end
+		// else
+		// begin
+			// if (in_AB_Pulse)  	// Note: in_AB_Pulse is same as BtnR_Pulse.
+			////					****** TODO  in Part 2 ******
+			////					Complete the lines below so that you deposit the value on switches
+			////					either in Ain or in Bin based on the value of the flag A_bar_slash_B. 
+			////					Also you need to toggle the value of the flag A_bar_slash_B.
+				// begin
+					// A_bar_slash_B <= ~ A_bar_slash_B;
+					// if (A_bar_slash_B == 1'b0)
+						// input_arr <= {Sw7, Sw6, Sw5, Sw4, Sw3, Sw2, Sw1, Sw0};
+					// else
+						// Bin <= {Sw7, Sw6, Sw5, Sw4, Sw3, Sw2, Sw1, Sw0};
+				// end
+		// end
+	// end
+	//reg [3:0] input_arr [63:0];
 	always @ (posedge sys_clk, posedge Reset)
-	begin
-		if(Reset)
-		begin
-			//initialize matrix to identity matrix
-			integer i, j;
-			for(i = 0; i < 8; i++) begin
-				for(j = 0; j < 8; j++) begin
-					if(i == j)
-						input_arr[i][i] <= 1;
-					else
-						input_arr[i][j] <= 0;
-				end
+	begin 
+		if (Reset) 
+            input_arr[0] <=  4'bX;
+		//do something
+		else if ( q_Enter == 1)
+		  begin : ArrayInputBlock
+		    reg [5:0] arrIndex;
+            arrIndex = 8*currRow + currColumn;
+            if ( enterPulse)
+                input_arr[arrIndex] <=  matrixInput;
+            
+            for(i = 0; i < 64; i=i+1) 
+            begin
+				{PackedInput_array[4*i+3],PackedInput_array[4*i+2],PackedInput_array[4*i+1],PackedInput_array[4*i]} <=  PackedInput_array [i];
 			end
-		end
-		else
-		begin
-			if (in_AB_Pulse)  	// Note: in_AB_Pulse is same as BtnR_Pulse.
-								// ****** TODO  in Part 2 ******
-								// Complete the lines below so that you deposit the value on switches
-								// either in Ain or in Bin based on the value of the flag A_bar_slash_B. 
-								// Also you need to toggle the value of the flag A_bar_slash_B.
-				begin
-					A_bar_slash_B <= ~ A_bar_slash_B;
-					if (A_bar_slash_B == 1'b0)
-						input_arr <= {Sw7, Sw6, Sw5, Sw4, Sw3, Sw2, Sw1, Sw0};
-					else
-						Bin <= {Sw7, Sw6, Sw5, Sw4, Sw3, Sw2, Sw1, Sw0};
-				end
-		end
+            
+		  end
 	end
 	
 	// the state machine module
-	ee354_GCD ee354_GCD_1(.Clk(sys_clk), .CEN(CEN_Pulse), .Reset(Reset), .Start(BtnL_Pulse), .Ack(BtnL_Pulse),  //Modified tp included correct states (final) TODO many things (chaged Start_Ack_Pulse to BtnL_Pulse
-						  .input_arr(input_arr), .Bin(Bin), .A(A), .B(B), .AB_GCD(AB_GCD), .i_count(i_count),
-						  .q_I(q_I), .q_Sub(q_Sub), .q_Mult(q_Mult), .q_Done(q_Done));
+	//ee354_GCD ee354_GCD_1(.Clk(sys_clk), .CEN(CEN_Pulse), .Reset(Reset), .Start(BtnL_Pulse), .Ack(BtnL_Pulse),  //Modified tp included correct states (final) TODO many things (chaged Start_Ack_Pulse to BtnL_Pulse
+	//					  .Enter(BtnR_Pulse)//added enter 
+	//					  .input_arr(input_arr), //.Bin(Bin), .A(A), .B(B), .AB_GCD(AB_GCD), .i_count(i_count), dont think I need any of this
+	//					  .q_I(q_I), .q_Sub(q_Sub), .q_Mult(q_Mult), .q_Done(q_Done)); 
+	//					  
+	ee354_FinalProject ee354_FinalProject1(.Clk(sys_clk), .Reset(Reset), .Start(BtnL_Pulse), .Ack(BtnL_Pulse),  //Modified tp included correct states (final) TODO many things (chaged Start_Ack_Pulse to BtnL_Pulse
+						  .input_arr_flat(PackedInput_array), .det(det), //.Bin(Bin), .A(A), .B(B), .AB_GCD(AB_GCD), .i_count(i_count), <-dont think I need any of this
+						  .q_I(q_I), .q_Enter(q_Enter), .q_Load(q_Load),.q_Comp(q_Comp), .q_Done(q_Done)); 
 
 //------------
 // OUTPUT: LEDS
+
+
+
+
+
+
 	
-	assign {Ld7, Ld6, Ld5, Ld4} = {q_I, q_Sub, q_Mult, q_Done}; //Modified tp included correct states (final) TODO
-	assign {Ld3, Ld2, Ld1, Ld0} = {BtnL, BtnU, BtnR, BtnD}; // Reset is driven by BtnC
+	assign {Ld7, Ld6, Ld5, Ld4,  Ld3} = {q_I, q_Enter, q_Load ,q_Comp, q_Done}; //Modified tp included correct states (final) TODO
+	assign {Ld2 ,Ld1, Ld0} = {BtnC, BtnL, BtnR}; // Reset is driven by BtnC
 	// Here
 	// BtnL = Start/Ack
 	// BtnU = Single-Step
@@ -186,38 +235,45 @@ ee354_debouncer #(.N_dc(28)) ee354_debouncer_0
 	//SSDs show Ain and Bin in initial state, A and B in subtract state, and GCD and i_count in multiply and done states.
 	// ****** TODO  in Part 2 ******
 	// assign y = s ? i1 : i0;  // an example of a 2-to-1 mux coding
-	// assign y = s1 ? (s0 ? i3: i2): (s0 ? i1: i0); // an example of a 4-to-1 mux coding
-	assign SSD3 = (q_Mult | q_Done) ? AB_GCD[7:4]  : q_I ? Ain[7:4] : A[7:4];
-	assign SSD2 = (q_Mult | q_Done) ? AB_GCD[3:0]  : q_I ? Ain[3:0] : A[3:0];
-	assign SSD1 = (q_Mult | q_Done) ? i_count[7:4] : q_I ? Bin[7:4] : B[7:4];
-	assign SSD0 = (q_Mult | q_Done) ? i_count[3:0] : q_I ? Bin[3:0] : B[3:0];
+	// assign y = s1 ? (s0 ? i3: i2): (s0 ? i1: i0); // an example of a 4-to-1 mux coding 
+	//TODO (final) 
+	assign SSD7 = (q_Done) ? det[31:28]  : 4'b0000;
+	assign SSD6 = (q_Done) ? det[27:24]  : 4'b0000;
+	assign SSD5 = (q_Done) ? det[23:20]  : 4'b0000;
+	assign SSD4 = (q_Done) ? det[19:16]  : 4'b0000;
+	assign SSD3 = (q_Done) ? det[15:12]  : {1'b0,currRow};
+	assign SSD2 = (q_Done) ? det[11:08]  : {1'b0,currColumn};
+	assign SSD1 = (q_Done) ? det[07:04]  : 4'b0000;
+	assign SSD0 = (q_Done) ? det[03:00]  : matrixInput;
 
 
 	// need a scan clk for the seven segment display 
-	// 191Hz (100 MHz / 2^19) works well
-	assign ssdscan_clk = DIV_CLK[19:18];
-	
-	assign An3	= !(~(ssdscan_clk[1]) && ~(ssdscan_clk[0]));  // when ssdscan_clk = 00    TODO: enable all the leds (FINAL)
-	assign An2	= !(~(ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 01
-	assign An1	=  !((ssdscan_clk[1]) && ~(ssdscan_clk[0]));  // when ssdscan_clk = 10
-	assign An0	=  !((ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 11
-	
-	
-	// close another four anodes
-	assign An7 = 1'b1;
-	assign An6 = 1'b1;
-	assign An5 = 1'b1;
-	assign An4 = 1'b1;
+	// 191Hz (100 MHz / 2^19) works well CHANGED (FINAL)
+	assign ssdscan_clk = DIV_CLK[19:17];
+	assign An0	= !( ~(ssdscan_clk[2]) && ~(ssdscan_clk[1]) && ~(ssdscan_clk[0]));  // when ssdscan_clk = 000
+	assign An1	= !(~(ssdscan_clk[2]) && ~(ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 001
+	assign An2	=  !(~(ssdscan_clk[2]) && (ssdscan_clk[1]) && ~(ssdscan_clk[0]));  // when ssdscan_clk = 010
+	assign An3	=  !(~(ssdscan_clk[2]) && (ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 011
+	 assign An4	= !((ssdscan_clk[2]) && ~(ssdscan_clk[1]) && ~(ssdscan_clk[0]));  // when ssdscan_clk = 100
+	 assign An5	= !((ssdscan_clk[2]) && ~(ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 101
+	 assign An6	=  !((ssdscan_clk[2]) && (ssdscan_clk[1]) && ~(ssdscan_clk[0]));  // when ssdscan_clk = 110
+	 assign An7	=  !((ssdscan_clk[2]) && (ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 111
 	
 	
-	always @ (ssdscan_clk, SSD0, SSD1, SSD2, SSD3)
+
+	
+	
+	always @ (ssdscan_clk, SSD0, SSD1, SSD2, SSD3, SSD4, SSD5, SSD6, SSD7) //removed old case structure
 	begin : SSD_SCAN_OUT
 		case (ssdscan_clk) 
-				  2'b00: SSD = SSD3;
-				  2'b01: SSD = SSD2;
-				  
-				  2'b10: SSD = SSD1;
-				  2'b11: SSD = SSD0;
+				  3'b000: SSD = SSD0;
+				  3'b001: SSD = SSD1;
+				  3'b010: SSD = SSD2;
+				  3'b011: SSD = SSD3;
+				  3'b100: SSD = SSD4;
+				  3'b101: SSD = SSD5;
+				  3'b110: SSD = SSD6;
+				  3'b111: SSD = SSD7;
 		endcase 
 	end
 	
